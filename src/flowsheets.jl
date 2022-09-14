bd2608f42123454c5594abfcba169f345077527b
Braam van Dyk <braam.vandyk@gmail.com>
2022-09-14 14:51:20 +0200

    Improve diagram styling

--- src/flowsheets.jl
+++ src/flowsheets.jl
@@ -174,7 +174,7 @@ function generateBFD(fs::Flowsheet)
         end
     end
 
-    mermaidlines = "%%{init: {'securityLevel': 'loose', 'theme': 'default', 'themeVariables': { 'fontSize': '14'}}}%%\ngraph LR\n"
+    mermaidlines = "%%{init: {'securityLevel': 'loose', 'theme': 'base', 'themeVariables': { 'fontSize': '14', 'darkMode': 'true', 'primaryColor': '#1e90ff', 'lineColor': '#1e90ff'}}}%%\ngraph LR\n"
     for stream in streams
         if stream[1] == " " # Feed stream
             mermaidline = "$(stream[2])([ ]) -->|$(stream[2])| $(stream[3])[$(stream[3])]"
