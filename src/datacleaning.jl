8f449da048ff8392be0f59c2b2afe67df15a3a71
braamvandyk <braam.vandyk@gmail.com>
2024-11-04 14:10:46 +0200

    WRap up smoothing

--- src/datacleaning.jl
+++ src/datacleaning.jl
@@ -1,5 +1,5 @@
 using Dates, Loess, Interpolations, Missings, TimeSeries, Statistics
-# using ChangePointDetection
+
 # # These are used only during testing
 using Plots, Distributions
 
@@ -21,12 +21,13 @@ function gendata(timestamps, period, fracfilled, fracdouble)
     # We use HoL since we need the x-axis to run from 0.0
     # The timestamps are also converted into a Float64 with hours since start
     HoL = calcHoL(timestamps)
+    basedata = zeros(Union{Float64, Missing}, length(times))
     data = zeros(Union{Float64, Missing}, length(times))
 
     for i in eachindex(HoL)
-        data[i] = sin(π*period*(HoL[i]/(Hour(endtime - starttime)/Hour(1))))
-        norm = Normal(0, 0.2*abs(data[i]))
-        data[i] += rand(norm)
+        basedata[i] = sin(π*period*(HoL[i]/(Hour(endtime - starttime)/Hour(1))))
+        norm = Normal(0, 0.2*abs(basedata[i]))
+        data[i] = basedata[i] + rand(norm)
     end
 
     len = length(data)
@@ -49,7 +50,7 @@ function gendata(timestamps, period, fracfilled, fracdouble)
         end
     end
     
-    return data
+    return data, basedata
 end
 
 function genstepdata(timestamps, period, fracfilled, fracdouble, stepsize=0.0)
@@ -91,7 +92,7 @@ end
 
 
 """
-    filldata(raw; allsmoothed=false, denoise=false, sensitivity = 2, loessspan=0.3, 
+    filldata(raw; allsmoothed=false, denoise=false, threshold = 2, α=0.3, 
         suggest_start=false, startvals=Float64[], suggest_end=false, endvals=Float64[])
 
 Fill a time series using LOESS with suggested start and end values or linear extrapolations.
@@ -103,7 +104,7 @@ If `denoise` is true, datapoints will be replaced with the smoothed value when `
 If `allsmoothed` is true, all values are smoothed using LOESS, otherwise only missings are filled.
 
 """
-function filldata(raw; allsmoothed=false, denoise=false, sensitivity = 2, loessspan=0.3, 
+function filldata(raw; fullsmooth=false, denoise=false, threshold = 2, α=0.3, 
     suggest_start=false, startvals=Float64[], suggest_end=false, endvals=Float64[])
 
     HoL = calcHoL(timestamp(raw))
@@ -139,11 +140,11 @@ function filldata(raw; allsmoothed=false, denoise=false, sensitivity = 2, loesss
         end
 
         # Fit a loess model to the data
-        model = loess(_HoL, _data; span=loessspan, degree=2)
+        model = loess(_HoL, _data; span=α, degree=2)
         
         fulldata[:, i] .= predict(model, HoL)
 
-        if allsmoothed
+        if fullsmooth
             data[:, i] .= fulldata[:, i]
         else
             # Fill in missing data only
@@ -154,7 +155,7 @@ function filldata(raw; allsmoothed=false, denoise=false, sensitivity = 2, loesss
             # Replace points that are too different from the smoothed value
             # Find the standard deviation for the difference between smoothed and original
             σ = std(fulldata[:, i] .- data[:, i])
-            data[:, i] .= ifelse.(abs.(fulldata[:, i] .- data[:, i]) .< (sensitivity * σ), data[:, i], fulldata[:, i])
+            data[:, i] .= ifelse.(abs.(fulldata[:, i] .- data[:, i]) .< (threshold * σ), data[:, i], fulldata[:, i])
         end
     end
     
@@ -167,30 +168,80 @@ starttime = DateTime(2023, 1, 1, 0, 0)
 endtime = DateTime(2023, 1, 12, 24, 0)
 times = starttime:Hour(6):endtime
 
-raw = TimeArray(times, gendata(times, 2, 0.75, 0.5), [:raw1])
+rawvals, purevals = gendata(times, 2, 0.75, 0.5)
+raw = TimeArray(times, rawvals, [:raw])
+pure = TimeArray(times, purevals, [:pure])
 # raw = TimeArray(times, genstepdata(times, 20, 0.75, 0.5, 10) .+ 50.0, [:raw1])
 
 sf1 = filldata(raw)
-scatter(sf1, leg=:bottomleft)
-# ylims!((0.0, 70.0))
-scatter!(raw)
-
-# profile = ChangePointDetection.lsdd_profile(values(sf1.raw1); window = 5)
-# cps = ChangePointDetection.changepoints(values(sf1.raw1), window = 10)
+rename!(sf1, [:default02])
 
 sf2 = filldata(raw, denoise = true)
-scatter(sf2)
-scatter!(raw)
+rename!(sf2, [:denoise02])
 
-sf3 = filldata(raw, allsmoothed = true, suggest_start=true, suggest_end=true, startvals=[0.0, 0.0], endvals=[0.0, 0.0])
-scatter(sf3)
-scatter!(raw)
+sf3 = filldata(raw, fullsmooth = true, suggest_start=true, suggest_end=true, startvals=[0.0, 0.0], endvals=[0.0, 0.0])
+rename!(sf3, [:fullsmooth02])
+
+sf4 = filldata(raw, α=0.5)
+rename!(sf4, [:default05])
+
+sf5 = filldata(raw, denoise = true, α=0.5)
+rename!(sf5, [:denoise05])
+
+sf6 = filldata(raw, fullsmooth = true, α=0.5, suggest_start=true, suggest_end=true, startvals=[0.0, 0.0], endvals=[0.0, 0.0])
+rename!(sf6, [:fullsmooth05])
+
+alldata = TimeSeries.merge(raw, sf1, sf2, sf3, sf4, sf5, sf6)
 
 # https://stats.lse.ac.uk/fryzlewicz/wbs/wbs.pdf
 
+# begin
+#     scatter(raw, ms=6, label="raw")
+#     scatter!(sf1, label="default", marker=:square)
+#     scatter!(sf2, label="denoise", marker=:diamond)
+#     scatter!(sf3, label="allsmoothed", ms=2)
+# end
+
 begin
-    scatter(raw, ms=6, label="raw")
-    scatter!(sf1, label="default", marker=:square)
-    scatter!(sf2, label="denoise", marker=:diamond)
-    scatter!(sf3, label="allsmoothed", ms=2)
+    alldata = TimeSeries.merge(raw, sf1, sf2, sf3, sf4, sf5, sf6)
+    l = @layout [a b c d;
+                 e f g h]
+
+    pltraw = let
+        scatter(alldata[:raw], leg=:bottomleft, size=(900, 400));
+        plot!(pure[:pure])
+    end;
+
+    pltdef02 = let 
+        scatter(alldata[:default02], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    pltnoise02 = let 
+        scatter(alldata[:denoise02], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    pltsmooth02 = let 
+        scatter(alldata[:fullsmooth02], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    pltdef05 = let 
+        scatter(alldata[:default05], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    pltnoise05 = let 
+        scatter(alldata[:denoise05], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    pltsmooth05 = let 
+        scatter(alldata[:fullsmooth05], leg=:bottomleft, size=(900, 400))
+        plot!(pure[:pure])
+    end;
+
+    plot(pltraw, pltdef02, pltnoise02, pltsmooth02, pltraw, pltdef05, pltnoise05, pltsmooth05, layout = l, size=(3600, 800))
 end
+savefig("cleandemo.png")
